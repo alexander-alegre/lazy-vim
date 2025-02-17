a5278d02bbb9a40c0579d5567b56bae9beee3f1a
Alexander Alegre <alexander.alegre@outlook.com>
2025-02-17 11:25:06 -0700

    updates

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- navigate buffers
+vim.keymap.set("n", "<Tab>", ":bn<CR>", { desc = "Next Buffer" })
+vim.keymap.set("n", "<S-Tab>", ":bp<CR>", { desc = "Previous Buffer" })
